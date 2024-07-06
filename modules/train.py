642f7ebe8831be7e9006f969e4239a0438f241a6
Tomas <tomhe37@gmail.com>
2024-07-06 15:54:29 +0300

    modified loss and added mlflow tracking

--- modules/train.py
+++ modules/train.py
@@ -1,5 +1,7 @@
 from torch import optim
+import torch.nn.functional as F
 import numpy as np
+
 
 from matplotlib import pyplot as plt
 
@@ -10,20 +12,14 @@ from datetime import datetime
 import pickle
 
 
+import mlflow
+
+
+
+
+
 torch.autograd.set_detect_anomaly(True)
 
-
-def normalize(x):
-    x_norm = (x - x.min()) / (x.max() - x.min())
-    
-    return x_norm
-
-def neg_cos_sim(p, z):  # negative cosine similarity   
-    z.detach()  # stop gradient
-    
-    p = normalize(p)
-    z = normalize(z)
-    return -(p*z).mean(dim=-1)
 
 # def neg_cos_sim(p, z):  # negative cosine similarity   
 #     z.detach()  # stop gradient
@@ -41,14 +37,16 @@ def fit(
     lr=0.0001,
     num_samples=100,
     device=torch.device('cpu'),
-    losses=[],
-    losses_neg=[],
     verbose=0,
     log_every=5,    # save loss value (in batches)
     save_every=1000, # model checkpoint (in epochs)
-    log_to_file=False,  # save losses to file to see progress while training
+    experiment_name=None,
+    mlflow_url="http://localhost:5000",
 ):
-    
+    mlflow.set_tracking_uri(mlflow_url)
+    exp_name = f"model_{str(datetime.now().date())}" if experiment_name is None else experiment_name
+    mlflow.set_experiment(exp_name)
+
     model.train()
     model.to_device(device)
     # optimizer = optim.SGD(model.parameters(), lr=lr)
@@ -74,6 +72,7 @@ def fit(
         try:
             res1 = model(peak1.unsqueeze(0))
             res2 = model(peak2.unsqueeze(0))
+            res2.detach()  # stop gradient
         except Exception as e:
             print(e)
             print(peak1.shape)
@@ -82,7 +81,9 @@ def fit(
         # ----- forward pass -----
         
         # ----- update weights -----
-        loss = pos_weight*neg_cos_sim(res1, res2) + 1 / (res1**2 + res2**2).mean()
+        regularization_factor = 1 / (res1**2).mean() + 1 / (res2**2).mean()  # to make sure it doesn't collapse to zero
+        # the negative at the start because we want the value to be low when res1, res2 are similar
+        loss = -pos_weight*F.cosine_similarity(res1, res2) + regularization_factor
         loss.backward()
         optimizer.step()
         # ----- update weights -----
@@ -105,6 +106,7 @@ def fit(
         try:
             res1 = model(peak1.unsqueeze(0))
             res2 = model(peak2.unsqueeze(0))
+            res2.detach()  # stop gradient
         except Exception as e:
             print(e)
             print(peak1.shape, label1)
@@ -112,43 +114,23 @@ def fit(
         # ----- forward pass -----
 
         # ----- update weights -----
-        loss_neg = -neg_weight*neg_cos_sim(res1, res2) + 1 / (res1**2 + res2**2).mean()
+        regularization_factor = 1 / (res1**2).mean() + 1 / (res2**2).mean()  # to make sure it doesn't collapse to zero
+        loss_neg = neg_weight*F.cosine_similarity(res1, res2) + regularization_factor
         loss_neg.backward()
         optimizer.step()
         # ----- update weights -----
         
         
 
+        if sample_idx%log_every == 0: 
+            with mlflow.start_run():
+                mlflow.log_metric("Loss Positive Examples", loss.item(), step=sample_idx)
+                mlflow.log_metric("Loss Positive Examples", loss_neg.item(), step=sample_idx)
 
-        if losses is not None and sample_idx%log_every == 0: 
-            losses.append(loss.item())
-            if log_to_file:
-                with open('./progress_log/pos_loss.pkl', 'wb') as f:
-                    pickle.dump(losses, f)
-        if losses_neg is not None and sample_idx%log_every == 0: 
-            losses_neg.append(loss_neg.item())
-            
-            if log_to_file:
-                with open('./progress_log/neg_loss.pkl', 'wb') as f:
-                    pickle.dump(losses_neg, f)
-
-        # ----- verbose stuff -----
-        if verbose>0: 
-            if sample_idx%log_every == 0: samples_iter.set_description(f'PosLoss: {loss.item():.4f} NegLoss: {loss_neg.item():.4f}')
-            samples_iter.update()
-
-        if verbose>1 and sample_idx%plot_every == 0:
-            # fig.clear()
-
-            # plt.plot(losses)
-            plt.semilogy(np.arange(len(losses))*log_every, losses)
-            plt.xlabel('#Samples')
-            plt.ylabel('Loss')
-            plt.show()
-            # fig.canvas.draw()
-            # fig.canvas.flush_events()
-        # time.sleep(0.1)
-        # ----- verbose stuff -----
+            # ----- verbose stuff -----
+            if verbose>0: 
+                samples_iter.set_description(f'PosLoss: {loss.item():.4f} NegLoss: {loss_neg.item():.4f}')
+                samples_iter.update()
 
         if sample_idx == 0:
             architecture_save_path = f'./model_checkpoints/model_{str(datetime.now().date())}.txt'
